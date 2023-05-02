e1197d03ea91c6fd79bfb69c1c85ccd5d28f6c72
LianQi-Kevin <18201584863@163.com>
2023-05-03 02:32:18 +0800

    * add basic grid

--- src/View/Chat.vue
+++ src/View/Chat.vue
@@ -1,20 +1,56 @@
 <template>
-<!--  <h1>GPT4</h1>-->
-  <div class="container">
-      <el-header>
-
-      </el-header>
-  </div>
+    <div class="container">
+        <div class="list">list</div>
+        <div class="chat">chat</div>
+        <div class="input">
+            <el-button>Clean Chat</el-button>
+            <el-input type="text" v-model="inputArea" clearable rows="4" show-word-limit autofocus>
+                <template #append>
+                    <el-button>Send</el-button>
+                </template>
+            </el-input>
+        </div>
+    </div>
 </template>
 
 <script>
-
 export default {
+    data() {
+        return {
+            inputArea: "",
+        }
+    },
     methods: {
     }
 }
 </script>
 
 <style scoped>
+.container {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: grid;
+    width: 100%;
+    height: 100%;
+    grid-template-areas: "list chat"
+  "list input";
+    grid-template-columns: 200px 1fr;
+    grid-template-rows: 1fr 100px;
+}
+.container > div {
+    border: 1px dashed #888;
+}
 
+.list {
+    grid-area: list;
+}
+.chat {
+    grid-area: chat;
+}
+.input {
+    grid-area: input;
+    display: flex;
+}
 </style>
